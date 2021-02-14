d0c9a5da734acd53c3b69ca0e579d9424de24001
maguilar15 <maguilar15@miners.utep.edu>
2021-02-14 10:14:24 -0700

    upload@Sun Feb 14 10:14:24 MST 2021

--- ProxyServer.py
+++ ProxyServer.py
@@ -1,19 +1,22 @@
 from socket import *
-import sys, os
 from threading import Thread
 from func import start_proxy_server
-
+import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print('Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server')
+    try:
+        if len(sys.argv) <= 1:
+            print('Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server')
+            sys.exit(2)
+        # The proxy server is listening at 8888
+        tcpSerSock = socket(AF_INET, SOCK_STREAM)
+        tcpSerSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        tcpSerSock.bind((sys.argv[1], 8888))
+        tcpSerSock.listen(100)
+        while 1:
+            t = Thread(name="CacheService",target=start_proxy_server, args=(tcpSerSock,),daemon=True)
+            t.start()
+            t.join()
+    except SyntaxError:
+        print(f"This script requires python 3.\nYour current python is {sys.version_info.major}.")
         sys.exit(2)
-    # The proxy server is listening at 8888
-    tcpSerSock = socket(AF_INET, SOCK_STREAM)
-    tcpSerSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    tcpSerSock.bind((sys.argv[1], 8888))
-    tcpSerSock.listen(100)
-    while 1:
-        t = Thread(name="Cache Service",target=start_proxy_server, args=(tcpSerSock,),daemon=True)
-        t.start()
-        t.join()
--- func.py
+++ func.py
@@ -1,4 +1,5 @@
-import os, requests
+from socket import *
+import os, requests, ssl
 
 def start_proxy_server(tcpServerSocket):
     print('Ready to serve...')
@@ -6,19 +7,21 @@ def start_proxy_server(tcpServerSocket):
     tcpCliSock, addr = tcpServerSocket.accept()
 
     print('Received a connection from:', addr)
-
-    message = tcpCliSock.recv(1024)
-    print(message)
-    # Extract the filename from the given message
-    filename = message.decode("utf-8").split("/")[1]
-    filename = filename.replace("HTTP", "")  # result: www.yahoo.com
-    print(f"Searching for: {filename}")
-    filetouse = filename.strip()
+    try:
+        message = tcpCliSock.recv(1024)
+        print(message.decode("utf-8"))
+        filename = message.decode("utf-8").split("/")[1]
+    except IndexError:
+        print("empty message")
     ## FILL IN HERE...
+    filename = filename.replace("HTTP", "").replace("www.","")  # result: www.yahoo.com
+    hostname = f"www.{filename}"
+    print(f"Searching for: {filename}, with hostname: {hostname}")
+    filetouse = hostname.strip()
     fileExist = "false"
     try:
-        if not os.path.exists(os.path.join(os.getcwd(), filename)):
-            print(f"File does not exist in cache, searching for filename={filename}")
+        if not os.path.exists(os.path.join(os.getcwd(), filetouse)):
+            print(f"File does not exist in cache, searching for filename={filetouse}")
             fileExist = "false"
             raise IOError
 
@@ -29,21 +32,24 @@ def start_proxy_server(tcpServerSocket):
         tcpCliSock.send("Connection: close\r\n".encode("utf-8"))
 
         ## FILL IN HERE...
-        print(f"{filename} is stored in cache.")
-        obj = open(filename,"r", encoding="utf8", errors='ignore').readlines()
-        print(f"Loading the web page {filename} with {len(obj)} number of bytes.")
+        print(f"{filetouse} is stored in cache.")
+        obj = open(filetouse,"r", encoding="utf8", errors='ignore').readlines()
+        print(f"Loading the web page {filetouse} with {len(obj)} number of bytes.")
         [tcpCliSock.send(e.encode("utf-8")) for e in obj]
     # Error handling for file not found in cache, need to talk to origin server and get the file
     except IOError:
         if fileExist == "false":
             ## FILL IN HERE...
-            hostname = filename.replace("www.","").strip()
-            requestData = f"GET / HTTP/1.1\r\nHost: {filename}\r\nConnection: keep-alive\r\n\r\n"
+
+            requestData = f"GET / HTTP/1.1\r\nHost: {hostname}\r\nConnection: keep-alive\r\n\r\n"
             try:
-                print(f"Fetching from Origin Server.........................Request Data\n{requestData}")
-                r = requests.get(f"https://{filetouse}",headers=None,stream=True)
-                print(f"Response Code: {r} for {filename}")
-                with open(filename,"wb") as f:
+                print(f"Fetching from Origin Server.................................Request Data\n")
+                headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+                           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                           }
+                r = requests.get(f"https://{filetouse}",headers=headers,stream=True)
+                print(f"Response Code: {r.status_code} for {filetouse}")
+                with open(filetouse,"wb") as f:
                     f.write(r.content)
                     tcpCliSock.send(r.content)
                     f.close()
@@ -55,3 +61,11 @@ def start_proxy_server(tcpServerSocket):
                 tcpCliSock.send("Content-Type:text/html\r\n".encode("utf-8"))
                 tcpCliSock.send("\r\n".encode("utf-8"))
     tcpCliSock.close()
+
+def handle_post_request(message=bytes):
+    # PARSE (Request Type, data)
+    p = message.decode("utf-8").split("\r\n")
+    p2 = p[0].strip().split(" ")[0].strip()
+    print(f"Request Type: {p2}")
+    print(f"POST DATA: {p[-1].strip()}")
+    return p2
